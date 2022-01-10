fff9814ef28cc4104fb16a966bfe157f62ff939d
raden-dimas012 <dimascode012@gmail.com>
2022-01-10 20:32:17 +0700

    project preparation

---
+++ lib/ui/pages/pages.dart
@@ -0,0 +1,15 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+// import 'package:food_market/models/models.dart';
+import 'package:food_market/shared/shared.dart';
+// import 'package:food_market/ui/widgets/widgets.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
+import 'package:supercharged/supercharged.dart';
+
+part 'general_page.dart';
+part 'main_page.dart';
