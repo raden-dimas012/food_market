8279c3043f865360839b82a45fc9891ff5100419
raden-dimas012 <dimascode012@gmail.com>
2022-01-19 09:34:42 +0700

    user cubit and sign in page

--- lib/cubit/user_state.dart
+++ lib/cubit/user_state.dart
@@ -8,3 +8,21 @@ abstract class UserState extends Equatable {
 }
 
 class UserInitial extends UserState {}
+
+class UserLoaded extends UserState {
+  final User user;
+
+  const UserLoaded(this.user);
+
+  @override
+  List<Object> get props => [user];
+}
+
+class UserLoadingFailed extends UserState {
+  final String message;
+
+  const UserLoadingFailed(this.message);
+
+  @override
+  List<Object> get props => [message];
+}
