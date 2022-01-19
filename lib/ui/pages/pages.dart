06905b5d53ac21e454c9417a38e32d434980a567
raden-dimas012 <dimascode012@gmail.com>
2022-01-19 09:24:38 +0700

    food cubit

--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -1,15 +1,34 @@
 import 'dart:math';
+import 'dart:io';
+import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-// import 'package:food_market/models/models.dart';
+import 'package:food_market/cubit/food_cubit.dart';
+import 'package:food_market/cubit/transaction_cubit.dart';
+import 'package:food_market/cubit/user_cubit.dart';
+import 'package:food_market/models/models.dart';
 import 'package:food_market/shared/shared.dart';
-// import 'package:food_market/ui/widgets/widgets.dart';
+import 'package:food_market/ui/widgets/widgets.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
+import 'package:provider/src/provider.dart';
 import 'package:supercharged/supercharged.dart';
 
-part 'general_page.dart';
 part 'main_page.dart';
+part 'general_page.dart';
+part 'sign_in_page.dart';
+// part 'sign_up_page.dart';
+// part 'address_page.dart';
+
+// part 'food_page.dart';
+// part 'food_details_page.dart';
+// part 'payment_page.dart';
+// part 'illustration_page.dart';
+// part 'succes_order_page.dart';
+// part 'succes_sign_up_page.dart';
+// part 'order_history_page.dart';
+// part 'profil_page.dart';
+// part 'payment_method_page.dart';
